b612373d002271fe45488548d20b07a6c2e8ddff
scriptalert(1)/script <62079712+tomer6517@users.noreply.github.com>
2020-03-12 00:41:15 +0200

    <a data-pjax="true" title="<</a>>aaaaaaaaa</a></a></a></a></a></a>  <</span>>bbbbbbb </span></span></span></span></span></span></span></span>" class="link-gray" href="/tomer6517/heyallll/commit/1f674669eac0246d50cf9430cb04010fbf10d68f">&lt;&lt;/a&gt;&gt;aaaaaaaaa&lt;/a&gt;&lt;/a&gt;&lt;/a&gt;&lt;/a&gt;&lt;/a&gt;&lt;/a&gt;</a>
    <a data-pjax="true" title="<</a>>aaaaaaaaa</a></a></a></a></a></a>
    
    <</span>>bbbbbbb
    </span></span></span></span></span></span></span></span>" class="link-gray" href="/tomer6517/heyallll/commit/1f674669eac0246d50cf9430cb04010fbf10d68f">&lt;&lt;/a&gt;&gt;aaaaaaaaa&lt;/a&gt;&lt;/a&gt;&lt;/a&gt;&lt;/a&gt;&lt;/a&gt;&lt;/a&gt;</a>

--- test.js
+++ test.js
@@ -10,3 +10,9 @@ bbbbbbbbbbb
 vvvvvvvvvvv
 ggggggggggg
 dsadsad
+
+
+<a data-pjax="true" title="<</a>>aaaaaaaaa</a></a></a></a></a></a>
+
+<</span>>bbbbbbb
+</span></span></span></span></span></span></span></span>" class="link-gray" href="/tomer6517/heyallll/commit/1f674669eac0246d50cf9430cb04010fbf10d68f">&lt;&lt;/a&gt;&gt;aaaaaaaaa&lt;/a&gt;&lt;/a&gt;&lt;/a&gt;&lt;/a&gt;&lt;/a&gt;&lt;/a&gt;</a>
